e071295efa7dbd6b62006afd31b6a75523bd2c23
ritanurhainiperhayani <ritanurhainip@gmail.com>
2026-09-23 20:36:47 +0800

    Add files via upload

---
+++ Python/11.Python Strings/Escape Characters.py
@@ -0,0 +1,2 @@
+# The escape character allows you to use double quotes when you normally would not be allowed:
+txt = "We are the so-called \"Vikings\" from the north."
---
+++ Python/12.Python Booleans/Code Challenge/Challenge Booleans.py
@@ -0,0 +1,11 @@
+# Print the result of 10 > 9
+print(10 > 9)
+
+# Print the result of 10 == 9
+print(10 == 9)
+
+# Print the result of bool("Hello")
+print(bool("Hello"))
+
+# Print the result of bool(0)
+print(bool(0))
